d3cec64f16d7477f780834c2a804ca6a5c47917d
CarnageOG <egetashviligiorgi9@gmail.com>
2026-04-29 17:36:27 +0400

    made routing

---
+++ src/app/products/details/[id]/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import styles from "./page.module.css";
+
+function page() {
+  return (
+    <div className={styles.div_layout}>
+      id
+    </div>
+  )
+}
+
+export default page;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,8 +1,11 @@
 "use client";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
+import {useRouter} from "next/navigation";
 
 function page() {
+    
+    const router = useRouter();
     const [products, setProducts] = useState ([]);
     const [loading, setLoading] = useState (true);
     const [error, setError] = useState(false);
@@ -35,14 +38,12 @@ function page() {
         <div className={styles.div_layout}>
             <div className={styles.div_wrapper}>
                 {products.map((product) => (
-                    <div key={product.id}>
-                        <div className={styles.div_products}>
-                            <h1 className={styles.product_title}>{product.title}</h1>
-                            <p>Price: {product.price} $</p>
-                            <p className={styles.product_des}>{product.description}</p>
-                            <img className={styles.product_img} src={product.image} alt={product.title} />
-                            <div className={styles.product_category}>{product.category}</div>
-                        </div>
+                    <div className={styles.div_products} key={product.id} onClick={() => router.push(`/products/details/${product.id}`)}>
+                        <h1 className={styles.product_title}>{product.title}</h1>
+                        <p>Price: {product.price} $</p>
+                        <p className={styles.product_des}>{product.description}</p>
+                        <img className={styles.product_img} src={product.image} alt={product.title} />
+                        <div className={styles.product_category}>{product.category}</div>
                     </div>
                 ))}
             </div>
@@ -50,4 +51,4 @@ function page() {
     );
 }
 
-export default page
+export default page;
