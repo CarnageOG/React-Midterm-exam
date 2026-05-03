45d2afb8a345ad0afecc06c0df3e6b91b956a10d
CarnageOG <egetashviligiorgi9@gmail.com>
2026-05-03 17:52:28 +0400

    made total in cart page

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -69,28 +69,43 @@ const Page = () => {
         setCart((prev) => prev.filter((item) => item.id !== id));
     };
 
+    const total = cart.reduce((sum, item) => {
+        return sum + item.price * item.quantity;
+    }, 0);
+
     return (
         <div className={styles.div_layout}>
             <div className={styles.cart_wrapper}>
                 <h1 className={styles.cart_h1}>Shoping cart</h1>
-                <div className={styles.div_cart}>
-                    {cart.map((item) => (
-                        <div className={styles.div_cart_sum} key={item.id}>
-                            <div className={styles.div_product}>
-                                <img src={item.image} alt={item.title} width={50} />
-                                <h3 className={styles.cart_h3}>{item.title}</h3>
+                <div className={styles.cart_layout}>
+                    <div className={styles.div_cart}>
+                        {cart.map((item) => (
+                            <div className={styles.div_cart_sum} key={item.id}>
+                                <div className={styles.div_product}>
+                                    <img src={item.image} alt={item.title} width={50} />
+                                    <h3 className={styles.cart_h3}>{item.title}</h3>
+                                </div>
+                                <div className={styles.div_quan}>
+                                    <button className={styles.cart_button} onClick={() => increaseQty(item.id)}>+</button>
+                                    <p>{item.quantity}</p>
+                                    <button className={styles.cart_button} onClick={() => decreaseQty(item.id)}>-</button>
+                                </div>
+                                <div className={styles.div_price}>
+                                    <p>{(item.price * item.quantity).toFixed(2)} $</p>
+                                    <button className={styles.cart_button} onClick={() => deleteItem(item.id)}>DELETE</button>
+                                </div>
                             </div>
-                            <div className={styles.div_quan}>
-                                <button className={styles.cart_button} onClick={() => increaseQty(item.id)}>+</button>
-                                <p>{item.quantity}</p>
-                                <button className={styles.cart_button} onClick={() => decreaseQty(item.id)}>-</button>
-                            </div>
-                            <div className={styles.div_price}>
-                                <p>{(item.price * item.quantity).toFixed(2)} $</p>
-                                <button className={styles.cart_button} onClick={() => deleteItem(item.id)}>DELETE</button>
-                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.div_sum}>
+                        <div className={styles.div_total}>
+                            <h4>Total</h4>
+                            <p>{total.toFixed(2)} $</p>
                         </div>
-                    ))}
+                        <div className={styles.div_buy}>
+                            BUY
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
