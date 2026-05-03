147ef07b0c1b4decbc0a6cc231c4f9fba4fe5014
CarnageOG <egetashviligiorgi9@gmail.com>
2026-05-03 14:39:15 +0400

    made fetch in cart page

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,11 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-function page() {
+const Page = () => {
+    const [cart, setCart] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const getCart = async () => {
+            try {
+                const res = await fetch("https://fakestoreapi.com/carts/1");
+                const result = await res.json();
+                setCart(result);
+            } catch (error) {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getCart();
+    }, []);
+
+    if (loading) {
+        return (
+        <div className={styles.error_loading}>LOADING</div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className={styles.error_loading}>Something went wrong</div>
+        );
+    }
+
     return (
         <div className={styles.div_layout}>
-            cart
+            {cart?.products?.map((item) => (
+                <div key={item.productId}>
+                    <h2>{item.productId}</h2>
+                    <p>{item.quantity}</p>
+                </div>
+            ))}
         </div>
     );
-}
+};
 
-export default page;
+export default Page;
