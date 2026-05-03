279d2e29dfbd2f3512ef95bdd228ff2e47dc5077
CarnageOG <egetashviligiorgi9@gmail.com>
2026-05-03 12:51:16 +0400

    made cart page

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+function page() {
+    return (
+        <div className={styles.div_layout}>
+            cart
+        </div>
+    );
+}
+
+export default page;
